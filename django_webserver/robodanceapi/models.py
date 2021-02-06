a5b9ff28e6b1448f22a954c82c589450b0d37fed
Michael Haar <michael.haar92@gmail.com>
2021-02-06 13:16:19 +0100

    feat: Added a model for robots
    We can test the model by running `python manage.py shell`. Inside the shell we can then use the following commands to create a new robot:
    ```python
    from robodanceapi.models import Robot
    r = Robot(name="Jammydevil", powermove="Hand Hops", experience=7, outOfOrder=False, avatar="https://robohash.org/jammydevil" )
    r.save()
    ```

--- django_webserver/robodanceapi/models.py
+++ django_webserver/robodanceapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Robot(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    powermove = models.TextField()
+    experience = models.IntegerField()
+    outOfOrder = models.BooleanField()
+    avatar = models.URLField()

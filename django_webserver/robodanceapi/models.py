e20bd7df788fa521e7a273f235dbf12474ba4593
Michael Haar <michael.haar92@gmail.com>
2021-02-06 21:33:38 +0100

    feat: Added API for /danceoffs
    The API endpoint is available under `http://localhost:8000/api/danceoffs`. It allows the user to list previous danceoffs and to create new danceoffs.
    I tried to match the API provided by parkside: https://challenge.parkside-interactive.com/docs/#/danceoffs

--- django_webserver/robodanceapi/models.py
+++ django_webserver/robodanceapi/models.py
@@ -8,3 +8,12 @@ class Robot(models.Model):
     experience = models.IntegerField()
     outOfOrder = models.BooleanField()
     avatar = models.URLField()
+
+
+class DanceOff(models.Model):
+    id = models.AutoField(primary_key=True)
+    winner = models.ForeignKey(
+        Robot, on_delete=models.CASCADE, related_name='wins')
+    loser = models.ForeignKey(
+        Robot, on_delete=models.CASCADE, related_name='loses')
+    dancedAt = models.DateTimeField(auto_now_add=True)
--- django_webserver/robodanceapi/views.py
+++ django_webserver/robodanceapi/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import permissions
-from .models import Robot
-from .serializers import RobotSerializer
+from rest_framework import viewsets, permissions
+from .models import Robot, DanceOff
+from .serializers import RobotSerializer, DanceOffSerializer
 
 
 class RobotViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows robots to be viewed or edited.
+    API endpoint that allows the user to list/create/receive/update/destroy 
+    robots
     """
     queryset = Robot.objects.all().order_by('-id')
     serializer_class = RobotSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class DanceOffViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows the user to list/create/receive/update/destroy 
+    danceoffs.
+    """
+    queryset = DanceOff.objects.all().order_by('-id')
+    serializer_class = DanceOffSerializer
+    # TODO use IsAuthenticatedOrReadOnly permission later
+    permission_classes = [permissions.AllowAny]
+
+    def get_serializer(self, *args, **kwargs):
+        """
+        Extends the parent's get_serializer() function and sets `many=true` 
+        if data is a list of objects.
+        """
+        if isinstance(kwargs.get("data", {}), list):
+            kwargs["many"] = True
+
+        return super(DanceOffViewSet, self).get_serializer(*args, **kwargs)
